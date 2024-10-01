9687e94e5fa3ad926251b7b07af2fca4a2b0170a
DESKTOP-5SQNL29\xkr12 <xkr123456@gmail.com>
2024-10-01 23:35:17 +0900

    트랙 로직

--- musicReco/polls/services.py
+++ musicReco/polls/services.py
@@ -18,5 +18,20 @@ def search_artists(artist_name, limit=10):
             'image_url': artist['images'][-1]['url'] if artist['images'] else '',  # 이미지가 없는 경우 예외 처리
         }
         artists_data.append(artist_data)
-    
     return artists_data
+
+def artist_top_tracks(artist_id):
+    artist_top_tracks = sp.artist_top_tracks(artist_id, country='KR')
+    searched_top_tracks = []
+
+    for top_track in artist_top_tracks['tracks']:
+        top_track_data = {
+            'name': top_track['name'],
+            'id': top_track['id'],
+            'image_url': top_track['album']['images'][-1]['url'] if top_track['images'] else '',
+            'popularity': top_track['top_track'],
+            'preview_url': top_track['preview_url']
+        }
+        searched_top_tracks.append(top_track_data)
+    return searched_top_tracks
+
